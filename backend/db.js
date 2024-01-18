a1de914cf2feda2a24573635374cd6fe33126cb3
Whycodex <neerajgupta12300@gmail.com>
2024-01-18 23:59:26 +0530

    Added database code

--- backend/db.js
+++ backend/db.js
@@ -1,1 +1,15 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+
+mongoose.connect(URL);
+
+const todoSchema = mongoose.Schema({
+  title: String,
+  description: String,
+  completed: Boolean,
+});
+
+const todo = mongoose.model("todos", todoSchema);
+
+module.exports = {
+  todo,
+};
